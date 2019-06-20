dcf90b63a2f9bdc4d632f2f7e179a3f7401084eb
Mayur Dhande <dhandepp@gmail.com>
2019-06-20 17:31:24 +0530

    files

---
+++ src/main/java/java8/lambda/TestPureLambdas.java
@@ -0,0 +1,17 @@
+package java8.lambda;
+
+import java.util.stream.IntStream;
+
+public class TestPureLambdas {
+    public static void main(String[] args){
+        // Create our own lambda function, and it tries to match the method we have in functional interface
+        MyFunctionalable function = () -> System.out.println("Hello");
+       // function.apply();
+
+        System.out.println(doSome().isOdd(3));
+    }
+
+    public static FunctionableTest doSome(){
+        return (i) -> i %2 == 1;
+    }
+}
